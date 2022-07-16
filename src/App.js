b83369f494ccf76f7c7c5e64e987e8a0e6a847b0
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-07-16 19:20:19 +0300

    create and style the weather container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
