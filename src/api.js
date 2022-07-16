ce6e8bb8c2c3b98edd6676950034f8e8eb3caa54
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-07-16 18:50:38 +0300

    fetching the data from server about city input

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f66893e5f9msh00fb9b8646ff60dp134dfajsn9736e2742d47',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
