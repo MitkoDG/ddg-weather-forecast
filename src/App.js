a7c45bbb4e863fa24dab14c15a6b6cf424a8a719
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-07-16 19:47:13 +0300

    Promiss.all
    fetching
    Current Weather
    
    3-hour Forecast 5 days

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState } from 'react';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
 import CurrentWeather from './components/current-weather/current-weather';
 import Search from './components/search/search';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(' ');
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+    .then(async (responce) => {
+      console.log(responce);
+      const weatherResponse = await responce[0].json()
+      const forecastResponse = await responce[1].json()
+
+      setCurrentWeather({city: searchData.label, ...weatherResponse});
+      setForecast({city: searchData.label, ...forecastResponse});
+    })
+    .catch((err) => console.log(err));
+
   }
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
